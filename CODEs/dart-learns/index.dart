04f8859d2735068dc25066fac421e3db8bcb31a8
leo <leo@8u8.cc>
2025-10-26 16:02:04 +0400

    docs: 示例更新

---
+++ CODEs/dart-learns/index.dart
@@ -0,0 +1,13 @@
+import 'dart:math';
+
+void main() {
+  print(3.14.floor());
+  print(3.14.ceil());
+  print(3.14.round());
+  print(3.14.truncate());
+
+  print(3.14.floorToDouble());
+  print(3.14.ceilToDouble());
+  print(3.14.roundToDouble());
+  print(3.14.truncateToDouble());
+}
